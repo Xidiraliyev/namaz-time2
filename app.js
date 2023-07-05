038ab2225122b3a1177ee02c9825b8492a482352
Xidiraliyev <100831276+Xidiraliyev@users.noreply.github.com>
2023-07-05 08:52:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", async (req, res) => {
     res.sendFile(path.join(__dirname, "./salom.html"));
 });
 
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log("server is running");
 });
